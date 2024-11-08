8f46b6ed5c3a3d03893418b1ef927557c195e85d
Luis Valenzuela <luisrav14@gmail.com>
2024-11-08 16:42:11 -0600

    build(AWS): S3 client config

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import morgan from 'morgan'
 import express from 'express'
+import fileUpload from 'express-fileupload'
 
 import router from './routes'
 import connectDB from './config/database'
@@ -12,6 +13,15 @@ app.use(morgan('dev'))
 // Middlewares
 app.use(cors())
 app.use(express.json())
+
+// Files upload config
+app.use(
+  fileUpload({
+    limits: { fileSize: 5 * 1024 * 1024 },
+    abortOnLimit: false,
+    createParentPath: true
+  })
+)
 
 // MongoDB connection
 connectDB()
